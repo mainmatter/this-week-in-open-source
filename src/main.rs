dcdda0859dd7bfd870d4076723e654469e38ec9d
Bartlomiej Dudzik <bartholomew.dudzik@gmail.com>
2022-04-05 15:38:36 +0200

    feat: use cli module

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::path::Path;
 
+mod cli;
+use cli::{args, Args};
+
 const BREAK_LINE: &str = r#"
 
 "#;
@@ -34,18 +37,6 @@ struct FileConfig {
     exclude: Vec<String>,
 }
 
-#[derive(Debug)]
-struct Arg(String, String);
-
-#[cfg_attr(test, derive(PartialEq))]
-#[derive(Debug)]
-struct Args {
-    users: Vec<String>,
-    date: String,
-    date_sign: String,
-    config_path: String,
-}
-
 #[cfg_attr(test, derive(PartialEq))]
 #[derive(Deserialize, Debug, Clone)]
 struct Item {
@@ -65,60 +56,6 @@ fn read_config_from_file<P: AsRef<Path>>(path: P) -> Result<FileConfig, Box<dyn 
     let config = serde_json::from_reader(reader)?;
 
     Ok(config)
-}
-
-fn read_args() -> Vec<Arg> {
-    let mut args = vec![];
-    for pair in env::args().skip(1) {
-        let split = pair.split("=").collect::<Vec<&str>>();
-
-        let name = split[0].to_string();
-        let value = if split.len() == 1 {
-            String::from("")
-        } else {
-            split[1].to_string()
-        };
-
-        args.push(Arg(name, value));
-    }
-
-    args
-}
-
-fn process_args(pairs: Vec<Arg>) -> Args {
-    let mut args = Args {
-        users: vec![],
-        date: String::from(""),
-        date_sign: String::from(""),
-        config_path: String::from(""),
-    };
-
-    for pair in pairs {
-        match (pair.0.as_str(), pair.1.as_str()) {
-            ("--users", value) => {
-                args.users.append(
-                    &mut value
-                        .split(",")
-                        .map(|user| user.to_string())
-                        .collect::<Vec<String>>(),
-                );
-            }
-            ("--date", value) => {
-                args.date = value.to_string();
-            }
-            ("-before", _) => args.date_sign = String::from("<"),
-            ("-after", _) => args.date_sign = String::from(">"),
-            ("--config-path", value) => args.config_path = value.to_string(),
-            (name, value) => println!("Could not handle argument {} with value {}", name, value),
-        }
-    }
-
-    if args.config_path.len() == 0 {
-        println!("--config-path is not provided.");
-        println!("This will result with unlabelled items.");
-    }
-
-    args
 }
 
 async fn get_prs(
@@ -265,7 +202,7 @@ fn format_items(items: &Vec<Item>) -> Vec<String> {
 async fn main() -> octocrab::Result<()> {
     let octocrab = initialize_octocrab().await?;
 
-    let args = process_args(read_args());
+    let args = args();
     match read_config_from_file(args.config_path.clone()) {
         Ok(config) => {
             let mut repos = config.labels.clone();
@@ -422,116 +359,5 @@ mod tests {
         );
 
         assert_eq!(expected, labels_result);
-    }
-
-    #[test]
-    fn it_processes_args() {
-        let expected = Args {
-            users: vec![],
-            date: "".to_string(),
-            date_sign: "".to_string(),
-            config_path: "".to_string(),
-        };
-
-        assert_eq!(expected, process_args(vec![]));
-    }
-
-    #[test]
-    fn it_processes_users_args() {
-        let expected = Args {
-            users: vec!["BobrImperator".to_string()],
-            date: "".to_string(),
-            date_sign: "".to_string(),
-            config_path: "".to_string(),
-        };
-
-        assert_eq!(
-            expected,
-            process_args(vec![Arg(
-                "--users".to_string(),
-                "BobrImperator".to_string()
-            )])
-        );
-    }
-
-    #[test]
-    fn it_processes_multiple_users_args() {
-        let expected = Args {
-            users: vec!["BobrImperator".to_string(), "mansona".to_string()],
-            date: "".to_string(),
-            date_sign: "".to_string(),
-            config_path: "".to_string(),
-        };
-
-        assert_eq!(
-            expected,
-            process_args(vec![Arg(
-                "--users".to_string(),
-                "BobrImperator,mansona".to_string()
-            )])
-        );
-    }
-
-    #[test]
-    fn it_processes_date_args() {
-        let expected = Args {
-            users: vec![],
-            date: "2022-02-18".to_string(),
-            date_sign: "".to_string(),
-            config_path: "".to_string(),
-        };
-
-        assert_eq!(
-            expected,
-            process_args(vec![Arg("--date".to_string(), "2022-02-18".to_string())])
-        );
-    }
-
-    #[test]
-    fn it_processes_after_args() {
-        let expected = Args {
-            users: vec![],
-            date: "".to_string(),
-            date_sign: ">".to_string(),
-            config_path: "".to_string(),
-        };
-
-        assert_eq!(
-            expected,
-            process_args(vec![Arg("-after".to_string(), "".to_string())])
-        );
-    }
-
-    #[test]
-    fn it_processes_before_args() {
-        let expected = Args {
-            users: vec![],
-            date: "".to_string(),
-            date_sign: "<".to_string(),
-            config_path: "".to_string(),
-        };
-
-        assert_eq!(
-            expected,
-            process_args(vec![Arg("-before".to_string(), "".to_string())])
-        );
-    }
-
-    #[test]
-    fn it_processes_config_path_args() {
-        let expected = Args {
-            users: vec![],
-            date: "".to_string(),
-            date_sign: "".to_string(),
-            config_path: "../config/location.json".to_string(),
-        };
-
-        assert_eq!(
-            expected,
-            process_args(vec![Arg(
-                "--config-path".to_string(),
-                "../config/location.json".to_string()
-            )])
-        );
     }
 }
